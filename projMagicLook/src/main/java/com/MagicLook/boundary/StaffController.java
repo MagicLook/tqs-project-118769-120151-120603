6e86b039f9f73c3337bc3ef1a2ed1e608173188b
Navi <ivanhoroshko68@ua.pt>
2025-12-05 06:39:01 +0000

    feat (Controller): Add StaffController

--- projMagicLook/src/main/java/com/MagicLook/boundary/StaffController.java
+++ projMagicLook/src/main/java/com/MagicLook/boundary/StaffController.java
@@ -1,10 +1,29 @@
 package com.MagicLook.boundary;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import jakarta.validation.Valid;
+
+import com.MagicLook.service.StaffService;
+import com.MagicLook.dto.*;
 
 @RestController
 @RequestMapping("magiclook")
 public class StaffController {
-    
+    private StaffService staffService;
+
+    public StaffController(StaffService staffService) {
+        this.staffService = staffService;
+    }
+
+    @PostMapping("/item")
+    public ResponseEntity<String> addItem(@Valid @RequestBody ItemDTO itemDTO) {
+        int result = staffService.addItem(itemDTO);
+
+        if (result != 0) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Já existe um item com essas características");
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("Item adicionado com sucesso.");
+    }
 }
