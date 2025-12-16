58ec2d762b2028a63d1e9c905c3651e985a4af22
Maria-Aleksandra <maria.korjenevskaya@ua.pt>
2025-12-16 04:23:56 +0000

    style (Project Overall): Modify Booking Repository

--- projMagicLook/src/main/java/com/MagicLook/repository/BookingRepository.java
+++ projMagicLook/src/main/java/com/MagicLook/repository/BookingRepository.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
-import com.MagicLook.data.*;
+import com.magiclook.data.*;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, UUID>{
