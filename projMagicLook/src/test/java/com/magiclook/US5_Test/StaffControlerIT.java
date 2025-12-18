bf1f90163610f743580e8d2c0cd80d687bd82cf2
Maria-Aleksandra <maria.korjenevskaya@ua.pt>
2025-12-18 03:31:01 +0000

    Melhorias no frontend

--- projMagicLook/src/test/java/com/magiclook/US5_Test/StaffControlerIT.java
+++ projMagicLook/src/test/java/com/magiclook/US5_Test/StaffControlerIT.java
@@ -56,7 +56,7 @@ class StaffControllerIT {
 
         @BeforeEach
         void setUp() {
-                seededUsername = "anasi";
+                seededUsername = "admin";
                 seededPassword = "admin123";
 
                 Staff staff = staffService.login(seededUsername, seededPassword);
