b5166c3903d38420410d915e64699b3959c6ff34
Navi <ivanhoroshko68@ua.pt>
2025-12-19 02:02:59 +0000

    Add Shop Repository

--- projMagicLook/src/main/java/com/magiclook/repository/ShopRepository.java
+++ projMagicLook/src/main/java/com/magiclook/repository/ShopRepository.java
@@ -4,7 +4,9 @@ import com.magiclook.data.Shop;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ShopRepository extends JpaRepository<Shop, Integer> {
-    
+    Optional<Shop> findByNameAndLocation(String name, String location);
 }
