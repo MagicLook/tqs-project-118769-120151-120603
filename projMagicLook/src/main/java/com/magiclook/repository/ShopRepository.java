776b10af157c68e8fdda378c32fb1074e91122d2
goncalo floro <goncalofloro@ua.pt>
2025-12-19 01:52:37 +0000

    fix(shop): errors

--- projMagicLook/src/main/java/com/magiclook/repository/ShopRepository.java
+++ projMagicLook/src/main/java/com/magiclook/repository/ShopRepository.java
@@ -3,8 +3,9 @@ package com.magiclook.repository;
 import com.magiclook.data.Shop;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface ShopRepository extends JpaRepository<Shop, Integer> {
-    
+    Optional<Shop> findByNameAndLocation(String name, String location);
 }
