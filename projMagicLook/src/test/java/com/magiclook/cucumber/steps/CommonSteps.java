12278cd1b6d723d6be1fea2392be588568c30351
Navi <ivanhoroshko68@ua.pt>
2026-01-30 01:44:43 +0000

    fix: Enable headless mode for Selenium in CI

--- projMagicLook/src/test/java/com/magiclook/cucumber/steps/CommonSteps.java
+++ projMagicLook/src/test/java/com/magiclook/cucumber/steps/CommonSteps.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import java.time.Duration;
 
 import io.cucumber.java.en.Given;
@@ -20,13 +21,21 @@ public class CommonSteps {
     @Before
     public void setup() {
         WebDriverManager.firefoxdriver().setup();
-        driver = new FirefoxDriver();
+
+        FirefoxOptions options = new FirefoxOptions();
+        // Enable headless mode for CI environments (no display)
+        if (System.getProperty("headless", "false").equals("true") || System.getenv("CI") != null) {
+            options.addArguments("--headless");
+        }
+
+        driver = new FirefoxDriver(options);
         wait = new org.openqa.selenium.support.ui.WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     public void setDriver(WebDriver driver) {
         this.driver = driver;
     }
+
     public void setWait(WebDriverWait wait) {
         this.wait = wait;
     }
@@ -63,18 +72,18 @@ public class CommonSteps {
     @Given("that Alice is on the Mulher page")
     public void alice_is_on_mulher_page() {
         driver.get("http://localhost:8080/magiclook/login");
-        
+
         WebElement usernameField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("username")));
         WebElement passwordField = driver.findElement(By.id("password"));
         WebElement loginButton = driver.findElement(By.id("login-submit"));
-        
+
         usernameField.sendKeys("maria");
         passwordField.sendKeys("maria?");
-        
+
         loginButton.click();
-        
+
         wait.until(ExpectedConditions.urlContains("magiclook/dashboard"));
-        
+
         WebElement mulherNav = wait.until(ExpectedConditions.elementToBeClickable(By.id("womenItemsLink")));
         mulherNav.click();
 
